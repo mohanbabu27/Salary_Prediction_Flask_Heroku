ec2a48e427a78c046140fea32461547d6f862166
mohanbabu27 <48299263+mohanbabu27@users.noreply.github.com>
2019-12-17 23:31:40 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -1,49 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 13 22:48:04 2019
-
-@author: tallurim
-"""
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('C:/Users/tallurim/Desktop/ML/Deployment_Flask_Master/hiring.csv')
+dataset = pd.read_csv('hiring.csv')
 
-dataset['experience'].fillna(0, inplace=True) #FE replace all nan values as 0
+dataset['experience'].fillna(0, inplace=True)
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True) #filling data sent with mean value
+dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
-X = dataset.iloc[:, :3] #We have 3 features in data set which are independant values
+X = dataset.iloc[:, :3]
 
-def convert_to_int(word):  #Converting words to inegers in exp feature
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,
-                 'seven':7, 'eight':8, 'nine':9, 'ten':10, 'eleven':11,
-                 'twelve':12, 'zero':0, 0: 0}
+#Converting words to integer values
+def convert_to_int(word):
+    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
+                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
     return word_dict[word]
 
-#apply word to int in to feature column using lambda function
-X['experience'] = X['experience'].apply(lambda x:  convert_to_int(x))
+X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
-#Assigning the Y value
 y = dataset.iloc[:, -1]
 
-#Splitting training and Test Set
-#Since its very small data set, will train our model with all available data
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
-#Fitting model with training data
+#Fitting model with trainig data
 regressor.fit(X, y)
 
-#Saving model to disk
+# Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
 
-#loading model to compare the results
+# Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2,9,6]]))
-
-
-
+print(model.predict([[2, 9, 6]]))
